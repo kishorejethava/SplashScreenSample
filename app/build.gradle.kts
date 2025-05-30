6f12c55d11938233abb18318d11dd221ea32fff1
Kishore Jethava <jethavakishore@gmail.com>
2025-05-30 17:23:10 +0530

    updated ci pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,16 @@ android {
     }
 
     buildTypes {
+        debug {
+            enableUnitTestCoverage = true
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
                     getDefaultProguardFile("proguard-android-optimize.txt"),
                     "proguard-rules.pro"
             )
+            enableUnitTestCoverage = false
         }
     }
 
